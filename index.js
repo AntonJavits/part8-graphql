6bf20991220da1f837bfcbc74eba511574481ee8
AntonJ <anton.javits@gmail.com>
2021-10-20 11:10:09 +0300

    8.7: Edit author born date

--- index.js
+++ index.js
@@ -117,7 +117,14 @@ const typeDefs = gql`
       published: Int!,
       genres: [String!]!
     ): Book
+
+    editAuthor(
+      name: String!,
+      setBornTo: Int
+    ): Author
+
   }
+
 `
 
 const resolvers = {
@@ -154,6 +161,13 @@ const resolvers = {
         authors = authors.concat(newAuthor)
       }
       return book
+    },
+    editAuthor: (root, args) => {
+      const author = authors.find(a => a.name === args.name)      
+      if (!author) { return null }
+      const updatedAuthor = { ...author, born: args.setBornTo }
+      authors = authors.map(a => a.name === args.name ? updatedAuthor : a)
+      return updatedAuthor
     }
   }
 }
